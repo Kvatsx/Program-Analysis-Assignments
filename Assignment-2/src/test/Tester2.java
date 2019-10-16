d75098fab2bc38a5c7b31b4d5430323c7a26f3ec
kaustavvats <kaustav16048@iiitd.ac.in>
2019-10-16 15:46:30 +0530

    A2 Final Upload

---
+++ Assignment-2/src/test/Tester2.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.util.Scanner;
+
+public class Tester2 {
+static int foo() {
+Scanner sc = new Scanner(System.in);
+int z;
+int a = sc.nextInt();
+int b = sc.nextInt();
+if (a > b) {
+a = 10;
+} else {
+b = 11;
+}
+int c = 10;
+while(a>5)
+{
+a--;
+c = 10 + c;
+}
+z = a + b + c;
+System.out.println(z);
+return c;
+}
+public static void main(String[] args) {
+int a = foo();
+}
+}
